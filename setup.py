fee4263ed1913c5a5e538f04dd2c2b6e39007874
Júlio Vaz <juliolopesvaz@gmail.com>
2019-04-29 20:03:48 -0300

    Add package setup files

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = ['requests', ]
+
+setup_requirements = ['pytest-runner', 'setuptools', 'wheel', 'bumpversion']
+
+test_requirements = ['pytest', 'coverage', ]
+
+setup(
+    author="Júlio Vaz",
+    author_email='jvaz@stone.com.br',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    description="Simple python authorization library.",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme,
+    include_package_data=True,
+    keywords='python_authorization',
+    name='python_authorization',
+    # packages=find_packages(include=['pyglobal_identity']),
+    packages=find_packages(exclude=['tests']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/julio-vaz/python-authorization',
+    version='2.1.0',
+    zip_safe=False,
+)
