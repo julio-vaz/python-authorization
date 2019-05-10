4152294ff7ba8913db61783a22a488b1eb973235
Júlio Vaz <juliolopesvaz@gmail.com>
2019-05-09 22:48:14 -0300

    Bumps version

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/julio-vaz/python-authorization',
-    version='1.0.2',
+    version='1.0.3',
     zip_safe=False,
 )
